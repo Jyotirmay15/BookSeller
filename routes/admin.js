4986b7ca0534537e9cc48a6e3bfbf9833d30d392
Jyotirmay <jyomishra99@gmail.com>
2020-03-20 01:05:59 +0530

    Basic Layout with pug templating engine

---
+++ routes/admin.js
@@ -0,0 +1,24 @@
+const express = require('express');
+
+const path = require('path');
+
+const rootDir = require('../util/path');
+
+const router = express.Router();
+
+const products = [];
+
+// /admin/add-product => GET
+router.get("/add-product", (req, res, next) => {
+  //console.log("In product middleware!");
+  res.render('add-product', {pageTitle: 'Add Product', path: '/admin/add-prouct'});
+});
+
+// /admin/add-product => POST 
+router.post("/add-product", (req, res, next) => {
+  products.push({ title: req.body.title });
+  res.redirect("/");
+});
+
+exports.routes = router;
+exports.products = products;
