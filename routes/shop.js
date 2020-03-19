c89ae21fd94df5d2bc094e5e577f52107e883381
Jyotirmay <jyomishra99@gmail.com>
2020-03-20 02:24:21 +0530

    Templating engine(handlebars)

--- routes/shop.js
+++ routes/shop.js
@@ -7,11 +7,16 @@ const adminData = require('./admin');
 
 const router = express.Router();
 
-router.get("/", (req, res, next) => {
-  // console.log('shop.js', adminData.products);
-  // res.sendFile(path.join(rootDir, 'views', 'shop.html'));
+router.get('/', (req, res, next) => {
   const products = adminData.products;
-  res.render('shop', {prods: products, pageTitle: 'Shop', path: '/'});
+  res.render('shop', {
+    prods: products,
+    pageTitle: 'Shop',
+    path: '/',
+    hasProducts: products.length > 0,
+    activeShop: true,
+    productCSS: true
+  });
 });
 
 module.exports = router;
